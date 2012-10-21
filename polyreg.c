3aad5932ef2e84c95bbc92e672cb7f5187032e22
Anh Cuong Nguyen <nacuong@live.com>
2012-10-21 01:08:41 -0700

    done debugging of dgefa

--- polyreg.c
+++ polyreg.c
@@ -13,7 +13,11 @@ void dmxpy(int n1, double y[], int n2, int ldm, double x[], double m[ldm][n2]);
 
 double ddot(int n, double dx[], int incx, double dy[], int incy);
 
+void tranpose(int n, double a[n][n]);
+
 int idamax(int n, double dx[], int incx);
+
+void aprint(char* sre, int n, double a[n]);
 
 int main() {
 	int i, i1, i2, j, job, k, m, n1; 
@@ -25,7 +29,7 @@ int main() {
 	double d1, dc[n], dc1[n], dv[n+1], dv1[n+1];
 	// fortran line 13
 	// r, x, v and v1 start are index 1
-	double c[n], r[n+1], x[n+1][n+1], v[n+1], v1[n+1], t1, t2, t3, t4;
+	double c[n], r[n+1], x[n][n], v[n], v1[n+1], t1, t2, t3, t4;
 
 	job = 0;
 
@@ -44,11 +48,12 @@ int main() {
 		dc[i1] = c[i1];
 	}
 
-	printf("input data = [ %g %g %g %g %g %g %g %g %g ] \n", dc[0], dc[1], dc[2], dc[3], dc[4], dc[5], dc[6], dc[7], dc[8]); 
+	aprint("input data =", 9, dc);
+	// printf("input data = [ %g %g %g %g %g %g %g %g %g ] \n", dc[0], dc[1], dc[2], dc[3], dc[4], dc[5], dc[6], dc[7], dc[8]); 
 
 	// calculate polynomial regression coefficients
-	for (i2 = 1; i2 <= n+1; i2++) {
-		for (i1 = 1; i1 <= n+1; i1++) {
+	for (i2 = 1; i2 < n+1; i2++) {
+		for (i1 = 1; i1 < n+1; i1++) {
 			t1 = 0.0;
 
 			for (k = 0; k < n; k++) {
@@ -60,7 +65,7 @@ int main() {
 				}
 			}
 
-			x[i1][i2] = t1;
+			x[i1-1][i2-1] = t1;
 		}
 	}
 
@@ -76,25 +81,29 @@ int main() {
 			}
 		}
 
-		v[i1] = t1;
+		v[i1-1] = t1;
 	}
 
 	// fortran line 70-71
+	aprint("x =", 81, &x[0][0]);
 	dgefa(n, n, x, ipvt, info);
+	aprint("x =", 81, &x[0][0]);
 	dgesl(n, n, x, ipvt, v, job);
 
 	t1 = 0.0;
 
 	for (i1 = 1; i1 < n+1; i1++) {
-		v1[i1] = rint(v[i1]);
-		t1 = fmax(t1, abs(v[i1]-v1[i1]));
-		dv[i1] = v[i1];
+		v1[i1] = rint(v[i1-1]);
+		t1 = fmax(t1, abs(v[i1-1]-v1[i1]));
+		dv[i1] = v[i1-1];
 		dv1[i1] = v1[i1];
 	}
 
 	printf("max deviation from integer value = %g \n", (double) t1);
-	printf("raw regression coefficients = [ %g %g %g %g %g %g %g %g %g ] \n", dv[1], dv[2], dv[3], dv[4], dv[5], dv[6], dv[7], dv[8], dv[9]); 
-	printf("rounded regression coefficients = [ %g %g %g %g %g %g %g %g %g ] \n", dv1[1], dv1[2], dv1[3], dv1[4], dv1[5], dv1[6], dv1[7], dv1[8], dv1[9]); 
+	aprint("raw regression coefficients =", 9, dv);
+	// printf("raw regression coefficients = [ %g %g %g %g %g %g %g %g %g ] \n", dv[1], dv[2], dv[3], dv[4], dv[5], dv[6], dv[7], dv[8], dv[9]); 
+	aprint("rounded regression coefficients =", 9, dv1);
+	// printf("rounded regression coefficients = [ %g %g %g %g %g %g %g %g %g ] \n", dv1[1], dv1[2], dv1[3], dv1[4], dv1[5], dv1[6], dv1[7], dv1[8], dv1[9]); 
 
 	// fortran line 86
 	d1 = 0.0;
@@ -116,7 +125,8 @@ int main() {
 	}
 
 	// fortran line 104
-	printf("regenerated data based on rounded regression coefficients = [ %g %g %g %g %g %g %g %g %g ] \n", dc1[0], dc1[1], dc1[2], dc1[3], dc1[4], dc1[5], dc1[6], dc1[7], dc1[8]); 
+	// printf("regenerated data based on rounded regression coefficients = [ %g %g %g %g %g %g %g %g %g ] \n", dc1[0], dc1[1], dc1[2], dc1[3], dc1[4], dc1[5], dc1[6], dc1[7], dc1[8]); 
+	aprint("regenerated data based on rounded regression coefficients =", 9, dc1);
 	printf("total error from original data = %g \n", d1);
 }
 
@@ -134,32 +144,42 @@ void dgefa(int lda, int n, double a[lda][n], int ipvt[], int info) {
 		 // find l = pivot index
 
 		// fortran line 176
-		l = idamax(n-k+1, &a[k][k], 1) + k - 1;
-		ipvt[k] = l;
+		// DEBUG
+		// if (k == 2) aprint("a =", 81, &a[0][0]);
+		l = idamax(n-k+1, &a[k-1][k-1], 1) + k - 1;
+		// printf("l = %d\n", l);
+		// END DEBUG
+		ipvt[k-1] = l;
 
 		// zero pivot implies this column already triangularized
-		if (a[l][k] == 0.0) goto l40;
+		if (a[l-1][k-1] == 0.0) goto l40;
 
+		// if (k == 1) aprint("a =", 81, &a[0][0]);
 		// interchange if neccessary
 		if (l == k) goto l10;
-		t = a [l][k];
-		a[l][k] = a[k][k];
-		a[k][k] = t;
+		t = a[k-1][l-1];
+		a[k-1][l-1] = a[k-1][k-1];
+		a[k-1][k-1] = t;
+		// if (k == 1) aprint("a =", 81, &a[0][0]);
 
 l10:
 		// compute multipliers
-		t = -1.0/a[k][k];
-		dscal(n-k,t, &a[k+1][k],1);
+		t = -1.0/a[k-1][k-1];
+		// if (k == 1) printf("k =%g\n", t);
+		// DEBUG
+		dscal(n-k,t, &a[k-1][k],1);
+		// if (k == 1) aprint("a =", 81, &a[0][0]);
 
 		// row elimination with column indexing
 		for (j = kp1; j < n+1; j++) {
-			t = a[l][j];
+			t = a[j-1][l-1];
 			if (l == k) goto l20;
-			a[l][j] = a[k][j];
-			a[k][j] = t;
+			a[j-1][l-1] = a[j-1][k-1];
+			a[j-1][k-1] = t;
 l20:
-			daxpy(n-k,t,&a[k+1][k],1,&a[k+1][j],1);
+			daxpy(n-k,t,&a[k-1][k],1,&a[j-1][k],1);
 		} // l30
+		// if (k == 1) aprint("a =", 81, &a[0][0]);
 		goto l50;
 l40:
 		info = k;
@@ -168,8 +188,8 @@ l50:
 	} // l60
 
 l70:
-	ipvt[n] = n;
-	if (a[n][n] == 0.0) info = n;
+	ipvt[n-1] = n;
+	if (a[n-1][n-1] == 0.0) info = n;
 	return;
 }
 
@@ -185,22 +205,26 @@ void dgesl(int lda, int n, double a[lda][n], int ipvt[], double b[], int job) {
 
 	if (nm1 < 1) goto l30;
 	for (k = 1; k < nm1+1; k++) {
-		l = ipvt[k];
-		t = b[l];
+		l = ipvt[k-1];
+		t = b[l-1];
 		if (l == k) goto l10;
-		b[l] = b[k];
-		b[k] = t;
+		b[l-1] = b[k-1];
+		b[k-1] = t;
 l10:
-		daxpy(n-k, t, &a[k+1][k], 1, &b[k+1], 1);
+		tranpose(n,a);
+		daxpy(n-k, t, &a[k][k-1], 1, &b[k], 1);
+		tranpose(n,a);
 	} // l20
 l30:
 	// now solve u*x = y
 	
 	for (kb = 1; kb < n + 1; kb++) {
 		k = n + 1 - kb;
-		b[k] = b[k]/a[k][k];
-		t = -b[k];
-		daxpy(k-1,t,&a[1][k],1,&b[1],1);
+		b[k-1] = b[k-1]/a[k-1][k-1];
+		t = -b[k-1];
+		tranpose(n,a);
+		daxpy(k-1,t,&a[0][k-1],1,&b[0],1);
+		tranpose(n,a);
 	} // l40
 	goto l100;
 
@@ -210,8 +234,10 @@ l50:
 	// first solve trans(u)*y = b
 
 	for (k = 1; k < n+1; k++) {
-		t = ddot(k-1, &a[1][k], 1, &b[1], 1);
-		b[k] = (b[k]-t)/a[k][k];
+		tranpose(n,a);
+		t = ddot(k-1, &a[0][k-1], 1, &b[0], 1);
+		tranpose(n,a);
+		b[k-1] = (b[k-1]-t)/a[k-1][k-1];
 	} // l60
 
 	// now solve trans(l)*x = y
@@ -220,12 +246,14 @@ l50:
 	// fortran line 322
 	for (kb = 1; kb < nm1+1; kb++) {
 		k = n - kb;
-		b[k] = b[k] + ddot(n-k,&a[k+1][k],1,&b[k+1],1);
-		l = ipvt[k];
+		tranpose(n,a);
+		b[k-1] = b[k-1] + ddot(n-k,&a[k][k-1],1,&b[k],1);
+		tranpose(n,a);
+		l = ipvt[k-1];
 		if (l == k) goto l70;
-		t = b[l];
-		b[l] = b[k];
-		b[k] = t;
+		t = b[l-1];
+		b[l-1] = b[k-1];
+		b[k-1] = t;
 l70: 
 		continue;
 	} // l80
@@ -254,7 +282,7 @@ void daxpy(int n, double da, double dx[], int incx, double dy[], int incy){
 	if (incx < 0) ix = (-n+1)*incx + 1;
 	if (incy < 0) iy = (-n+1)*incy + 1;
 	for (i = 1; i < n + 1; i++) {
-		dy[iy] = dy[iy] + da*dx[ix];
+		dy[iy-1] = dy[iy-1] + da*dx[ix-1];
 		ix = ix + incx;
 		iy = iy + incy;
 	} // l10
@@ -263,7 +291,7 @@ void daxpy(int n, double da, double dx[], int incx, double dy[], int incy){
 	// code for both increments equal to 1
 l20:
 	for (i = 1; i < n + 1; i ++) {
-		dy[i] = dy[i] + da*dx[i];
+		dy[i-1] = dy[i-1] + da*dx[i-1];
 	} // l30
 
 	return;
@@ -287,7 +315,7 @@ double ddot(int n, double dx[], int incx, double dy[], int incy) {
 	if (incx < 0) ix = (-n+1)*incx + 1;
 	if (incy < 0) iy = (-n+1)*incy + 1;
 	for (i = 1; i < n+1; i++) {
-		dtemp = dtemp + dx[ix]*dy[iy];
+		dtemp = dtemp + dx[ix-1]*dy[iy-1];
 		ix = ix + incx;
 		iy = iy + incy;
 	} // l10
@@ -298,7 +326,7 @@ double ddot(int n, double dx[], int incx, double dy[], int incy) {
 	
 l20:
 	for (i = 1; i < n + 1; i++) {
-		dtemp = dtemp + dx[i]*dy[i];
+		dtemp = dtemp + dx[i-1]*dy[i-1];
 	} // l30
 	res = dtemp;
 	return res;
@@ -315,14 +343,14 @@ void dscal(int n, double da, double dx[], int incx) {
 	// code for increment not equal to 1
 	nincx = n*incx;
 	for (i = 1; i < nincx + 1; i += incx) {
-		dx[i] = da*dx[i];
+		dx[i-1] = da*dx[i-1];
 	} // l10
 	return;
 
 	// code for increment equal to 1
 l20:
 	for (i = 1; i < n + 1; i ++) {
-		dx[i] = da*dx[i];
+		dx[i-1] = da*dx[i-1];
 	} // l30
 
 	return;
@@ -335,6 +363,7 @@ int idamax(int n, double dx[], int incx){
 	double dmax;
 	int res;
 	int i, ix;
+	// aprint("idamax dx =", n, dx);
 
 	res = 0;
 	if (n < 1) return res;
@@ -344,12 +373,12 @@ int idamax(int n, double dx[], int incx){
 
 	// code for increment not equal to 1
 	ix = 1;
-	dmax = abs(dx[1]);
+	dmax = abs(dx[1-1]);
 	ix = ix + incx;
 	for (i = 2; i < n+1; i++) {
-		if (abs(dx[ix]) < dmax) goto l5;
+		if (abs(dx[ix-1]) < dmax) goto l5;
 		res = i;
-		dmax = abs(dx[ix]);
+		dmax = abs(dx[ix-1]);
 l5:
 		ix = ix + incx;
 	} // l10
@@ -357,14 +386,16 @@ l5:
 
 	// code for increment equal to 1
 l20:
-	dmax = abs(dx[1]);
+	dmax = fabs(dx[1-1]);
 	for (i = 2; i < n+1; i++) {
-		if (abs(dx[i]) < dmax) goto l30;
+		if (fabs(dx[i-1]) < dmax) goto l30;
 		res = i;
-		dmax = abs(dx[i]);
-	} // l30
+		dmax = fabs(dx[i-1]);
 
 l30:
+		continue;
+	} // l30
+
 	return res;
 }
 
@@ -383,4 +414,28 @@ void dmxpy(int n1, double y[], int n2, int ldm, double x[], double m[ldm][n2]) {
 	} // l200
 
 	return;
+}
+
+void aprint(char* str, int n, double a[n]) {
+	printf("%s\n", str);
+	int i;
+	for (i = 0; i < n; i+=4) {
+		printf("\t%g", a[i]);
+		if (i+1 < n) printf("\t%g", a[i+1]);
+		if (i+2 < n) printf("\t%g", a[i+2]);
+		if (i+3 < n) printf("\t%g", a[i+3]);
+		printf("\n");
+	}
+}
+
+void tranpose(int n, double a[n][n]) {
+	int i;
+	int j;
+	for (i = 0; i < n; i++) {
+		for (j = i + 1; j < n; j++) {
+			double save = a[i][j];
+			a[i][j] = a[j][i];
+			a[j][i] = save;
+		}
+	}
 }
